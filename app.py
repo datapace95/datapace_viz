a02fc23708bacf9a403e2968f9eaddbde9fb2c71
Julien GAUCHER <julien.gaucher@alltricks.com>
2025-05-29 23:05:09 +0200

    rm useless pages

--- app.py
+++ app.py
@@ -5,12 +5,7 @@ pages = {
     "": [
         st.Page("pages/home.py", title="Homepage", icon="🏠", default=True),
         st.Page("pages/split_by_zones.py", title="Check temps par zone", icon="🤖"),
-        st.Page("pages/heartrate_watts_ratio.py", title="Evolution ratio watts/fc", icon="💼"),
-        # st.Page("studies.py", title="Etudes", icon="🎓"),
-        # st.Page("skills.py", title="Compétences", icon="🔧"),
-        # st.Page("hobbies.py", title="Hobbies", icon="🚴"),
-        # st.Page("perso_project.py", title="Projet perso", icon="🧑🏽"),
-        # st.Page("why_streamlit.py", title="Pourquoi ce CV sur Streamlit ?", icon="❓")
+        st.Page("pages/heartrate_watts_ratio.py", title="Evolution ratio watts/fc", icon="💼")
     ],
 }
 
